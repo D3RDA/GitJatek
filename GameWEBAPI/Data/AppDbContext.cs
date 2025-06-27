cc791e1f52a4274de75a7664db2c4a7e58be3182
Derda Dominik <MRD3RDA@gmail.com>
2025-06-27 16:39:40 +0200

    Jatek fajlok feltoltese

---
+++ GameWEBAPI/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Game.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Game.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Character> Characters { get; set;}
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            :base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Character>().HasData(
+                new Character { Id = 1, Name = "Asmond", Class= "Mage", Dmg = 12, Hp = 100 },
+                new Character { Id = 2, Name = "Loki", Class = "Warrior", Dmg = 3, Hp = 100},
+                new Character { Id = 3, Name = "Trevor", Class = "Assasin", Dmg = 8, Hp = 100 }
+            );
+        }
+    }
+}
---
+++ GameWEBAPI/Models/Character.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Game.Models
+{
+    public class Character
+    {
+        
+            [Key]
+            public int Id { get; set; }
+
+            [Required]
+            [StringLength(100)]
+            public string Name { get; set; }
+
+            [StringLength(500)]
+            public string Class { get; set; }
+
+        [Required]
+        public int Dmg { get; set; } = 100;
+
+        public int Hp { get; set; } = 10;
+
+            public DateTime CreatedAt { get; set; } = DateTime.Now;
+        }
+    }
